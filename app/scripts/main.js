ae537e3665d533b622b30d3c55839d47efd89876
Allan Monzon <allanjmonzon@gmail.com>
2015-02-02 05:47:07 -0500

    adds scrollTop

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,27 @@
-console.log('The Iron Yard Rocks');
+$(document).ready(function(){
+
+	$('.abt').click(function() {
+		$('html, body').animate({
+			scrollTop: $('.about').offset().top -20
+		}, 2000);
+	});
+
+	$('.wrk').click(function() {
+		$('html, body').animate({
+			scrollTop: $('.work').offset().top -80
+		}, 2000);
+	});
+
+	$('.ctc').click(function() {
+		$('html, body').animate({
+			scrollTop: $('.contact').offset().top -80
+		}, 3000);
+	});
+
+	$('.arrow').click(function() {
+		$('html, body').animate({
+			scrollTop: $('header').offset().top -80
+		}, 3000);
+	});
+
+});
